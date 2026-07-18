cf926ffe0d7b9db6051f7ee4ccfbda46eb1d3630
Haasini Bingi <haasinibingi@gmail.com>
2026-07-18 11:59:06 +0530

    added toggle user feature

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,29 +1,35 @@
 // alert("Your first website")
-console.log("HELLO JS")
+console.log("hello from JS!")
 
-data={
-    "Name" : "John";
-    "Gender" : "Male";
-}
-
-
-
-var users=[
+var users = [
     {
-        "Name":"John Doe";
-        "Gender":"Male";
-        "img":"john.png"; 
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
     },
     {
-       "Name":"Jane Doe";
-       "Gender":"Female";
-       "img":"jane.png";  
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
     }
 ]
 
-var id=0;
+var id = 0;
 
 function toggleUser(){
-    id=(id+1)%2
-    
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
 }
