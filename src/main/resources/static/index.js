0003923b839fff6d24cd6611c6e48c9e938c5f81
Haasini Bingi <haasinibingi@gmail.com>
2026-07-18 11:41:48 +0530

    changed shadows appearance

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,29 @@
 // alert("Your first website")
 console.log("HELLO JS")
+
+data={
+    "Name" : "John";
+    "Gender" : "Male";
+}
+
+
+
+var users=[
+    {
+        "Name":"John Doe";
+        "Gender":"Male";
+        "img":"john.png"; 
+    },
+    {
+       "Name":"Jane Doe";
+       "Gender":"Female";
+       "img":"jane.png";  
+    }
+]
+
+var id=0;
+
+function toggleUser(){
+    id=(id+1)%2
+    
+}
